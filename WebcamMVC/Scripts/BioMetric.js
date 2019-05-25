73b2cbb41be9e54870f83d8ac6c4630e7e60dad5
rjporosh <poroshScientist@outlook.com>
2019-05-25 19:54:51 +0600

    Edited need to test Now

--- WebcamMVC/Scripts/BioMetric.js
+++ WebcamMVC/Scripts/BioMetric.js
@@ -4,9 +4,11 @@
 
 
 });
-
-function GetSupplierForSelectList() {
-
+var template_2 = "";
+var template_1 = "";
+var singleData = {};
+function MatchWithAllData() {
+   
     $.ajax({
 
         url: "/Home/GetAllUser/",
@@ -20,121 +22,12 @@ function GetSupplierForSelectList() {
 
             // get json data list
             $.each(data, function (key, obj) {
+                debugger;
+                 template_2 = obj.TemplateBase64;
+                 singleData = obj;
 
-                var template_1 = obj.TemplateBase64;
-                var template_2 = "";
-
-                function SuccessFunc1(result) {
-                    //if (result.ErrorCode == 0) {
-                    //    /* 	Display BMP data in image tag
-                    //        BMP data is in base 64 format
-                    //    */
-                    //    if (result != null && result.BMPBase64.length > 0) {
-                    //        document.getElementById('FPImage1').src = "data:image/bmp;base64," + result.BMPBase64;
-                    //    }
-                    //    template_1 = result.TemplateBase64;
-                    //}
-                    //else {
-                    //    alert("Fingerprint Capture Error Code:  " + result.ErrorCode + ".\nDescription:  " + ErrorCodeToString(result.ErrorCode) + ".");
-                    //}
-                    var template_1 = obj.TemplateBase64;
-                }
-
-                function SuccessFunc2(result) {
-                    if (result.ErrorCode == 0) {
-                        /* 	Display BMP data in image tag
-                            BMP data is in base 64 format
-                        */
-                        if (result != null && result.BMPBase64.length > 0) {
-                            document.getElementById('FPImage2').src = "data:image/bmp;base64," + result.BMPBase64;
-                        }
-                        template_2 = result.TemplateBase64;
-                    }
-                    else {
-                        alert("Fingerprint Capture Error Code:  " + result.ErrorCode + ".\nDescription:  " + ErrorCodeToString(result.ErrorCode) + ".");
-                    }
-                }
-
-                function ErrorFunc(status) {
-                    /*
-                        If you reach here, user is probabaly not running the
-                        service. Redirect the user to a page where he can download the
-                        executable and install it.
-                    */
-                    alert("Check if SGIBIOSRV is running; status = " + status + ":");
-                }
-
-                function CallSGIFPGetData(successCall, failCall) {
-                    var uri = "https://localhost:8443/SGIFPCapture";
-                    var xmlhttp = new XMLHttpRequest();
-                    xmlhttp.onreadystatechange = function () {
-                        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                            fpobject = JSON.parse(xmlhttp.responseText);
-                            successCall(fpobject);
-                        }
-                        else if (xmlhttp.status == 404) {
-                            failCall(xmlhttp.status)
-                        }
-                    }
-                    xmlhttp.onerror = function () {
-                        failCall(xmlhttp.status);
-                    }
-                    var params = "Timeout=" + "10000";
-                    params += "&Quality=" + "50";
-                    params += "&licstr=" + encodeURIComponent(secugen_lic);
-                    params += "&templateFormat=" + "ISO";
-                    xmlhttp.open("POST", uri, true);
-                    xmlhttp.send(params);
-                }
-
-                function matchScore(succFunction, failFunction) {
-                    if (template_1 == "" || template_2 == "") {
-                        alert("Please scan two fingers to verify!!");
-                        return;
-                    }
-                    var uri = "https://localhost:8443/SGIMatchScore";
-
-                    var xmlhttp = new XMLHttpRequest();
-                    xmlhttp.onreadystatechange = function () {
-                        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                            fpobject = JSON.parse(xmlhttp.responseText);
-                            succFunction(fpobject);
-                        }
-                        else if (xmlhttp.status == 404) {
-                            failFunction(xmlhttp.status)
-                        }
-                    }
-
-                    xmlhttp.onerror = function () {
-                        failFunction(xmlhttp.status);
-                    }
-                    var params = "template1=" + encodeURIComponent(template_1);
-                    params += "&template2=" + encodeURIComponent(template_2);
-                    params += "&licstr=" + encodeURIComponent(secugen_lic);
-                    params += "&templateFormat=" + "ISO";
-                    xmlhttp.open("POST", uri, false);
-                    xmlhttp.send(params);
-                }
-
-                function succMatch(result) {
-                    var idQuality = document.getElementById("quality").value;
-                    if (result.ErrorCode == 0) {
-                        if (result.MatchingScore >= idQuality)
-                            alert("MATCHED ! (" + result.MatchingScore + ")");
-
-                        else
-                            alert("NOT MATCHED ! (" + result.MatchingScore + ")");
-                    }
-                    else {
-                        alert("Error Scanning Fingerprint ErrorCode = " + result.ErrorCode);
-                    }
-                }
-
-                function failureFunc(error) {
-                    alert("On Match Process, failure has been called");
-                }
-
-
+                 matchScore(succMatch, failureFunc);
+                 
             });
 
            
@@ -149,7 +42,118 @@ function GetSupplierForSelectList() {
 
 };
 
+function SuccessFunc1(result) {
+    if (result.ErrorCode == 0) {
+        /* 	Display BMP data in image tag
+            BMP data is in base 64 format
+        */
+        if (result != null && result.BMPBase64.length > 0) {
+            document.getElementById('FPImage1').src = "data:image/bmp;base64," + result.BMPBase64;
+        }
+        template_1 = result.TemplateBase64;
+    }
+    else {
+        alert("Fingerprint Capture Error Code:  " + result.ErrorCode + ".\nDescription:  " + ErrorCodeToString(result.ErrorCode) + ".");
+    }
+     template_1 = obj.TemplateBase64;
+}
 
+//function SuccessFunc2(result) {
+//    if (result.ErrorCode == 0) {
+//        /* 	Display BMP data in image tag
+//            BMP data is in base 64 format
+//        */
+//        if (result != null && result.BMPBase64.length > 0) {
+//            document.getElementById('FPImage2').src = "data:image/bmp;base64," + result.BMPBase64;
+//        }
+//        template_2 = result.TemplateBase64;
+//    }
+//    else {
+//        alert("Fingerprint Capture Error Code:  " + result.ErrorCode + ".\nDescription:  " + ErrorCodeToString(result.ErrorCode) + ".");
+//    }
+//}
+
+function ErrorFunc(status) {
+    /*
+        If you reach here, user is probabaly not running the
+        service. Redirect the user to a page where he can download the
+        executable and install it.
+    */
+    alert("Check if SGIBIOSRV is running; status = " + status + ":");
+}
+
+function CallSGIFPGetData(successCall, failCall) {
+    var uri = "https://localhost:8443/SGIFPCapture";
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            fpobject = JSON.parse(xmlhttp.responseText);
+            successCall(fpobject);
+        }
+        else if (xmlhttp.status == 404) {
+            failCall(xmlhttp.status)
+        }
+    }
+    xmlhttp.onerror = function () {
+        failCall(xmlhttp.status);
+    }
+    var params = "Timeout=" + "10000";
+    params += "&Quality=" + "50";
+    params += "&licstr=" + encodeURIComponent(secugen_lic);
+    params += "&templateFormat=" + "ISO";
+    xmlhttp.open("POST", uri, true);
+    xmlhttp.send(params);
+}
+
+function matchScore(succFunction, failFunction) {
+    if (template_1 == "" || template_2 == "") {
+        alert("Please scan two fingers to verify!!");
+        return;
+    }
+    var uri = "https://localhost:8443/SGIMatchScore";
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            fpobject = JSON.parse(xmlhttp.responseText);
+            succFunction(fpobject);
+        }
+        else if (xmlhttp.status == 404) {
+            failFunction(xmlhttp.status)
+        }
+    }
+
+    xmlhttp.onerror = function () {
+        failFunction(xmlhttp.status);
+    }
+    var params = "template1=" + encodeURIComponent(template_1);
+    params += "&template2=" + encodeURIComponent(template_2);
+    params += "&licstr=" + encodeURIComponent(secugen_lic);
+    params += "&templateFormat=" + "ISO";
+    xmlhttp.open("POST", uri, false);
+    xmlhttp.send(params);
+}
+
+function succMatch(result) {
+    var idQuality = document.getElementById("quality").value;
+    if (result.ErrorCode == 0) {
+        if (result.MatchingScore >= idQuality)
+        {
+            alert("MATCHED ! (" + result.MatchingScore + ")");
+            document.getElementById("Userpic").src = singleData.Userpic;
+        }
+
+        else
+            alert("NOT MATCHED ! (" + result.MatchingScore + ")");
+    }
+    else {
+        alert("Error Scanning Fingerprint ErrorCode = " + result.ErrorCode);
+    }
+}
+
+function failureFunc(error) {
+    alert("On Match Process, failure has been called");
+}
 var fingerpos = ["LEFT_THUMB", "LEFT_INDEX", "LEFT_MIDDLE", "LEFT_RING", "LEFT_LITTLE",
 				"RIGHT_THUMB", "RIGHT_INDEX", "RIGHT_MIDDLE", "RIGHT_RING", "RIGHT_LITTLE"];
 var service_handle = "";
